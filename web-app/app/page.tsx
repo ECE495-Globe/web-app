d0678ecdcf4ffa8e213cdfaa6057135a14c65435
Volodymyr Akulov <onc736@usask.ca>
2026-03-23 22:20:37 -0600

    removed useless html code

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useState } from 'react';
 import PressButton from "./components/PressButton";
 
@@ -90,18 +89,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col gap-6 py-20 px-16 bg-white dark:bg-black">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Config
-          </h1>
         
         {/* Data Source Buttons */}
         <div className="flex flex-col gap-2">
@@ -270,32 +258,7 @@ export default function Home() {
             center.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Send Link
-          </a>
-        </div>
+    
       </main>
     </div>
   );
